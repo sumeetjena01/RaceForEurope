701eedfe3006abaff4b3493b4d46b0f8322bb0cd
Sumeet <73847665+sumeetjena01@users.noreply.github.com>
2025-04-21 17:03:21 -0400

    Forest 2-1 Spurs

--- src/App.js
+++ src/App.js
@@ -21,10 +21,9 @@ const teams = [
   },
   {
     name: "Forest",
-    currentPoints: 57,
-    goalDifference: 13,
+    currentPoints: 60,
+    goalDifference: 14,
     fixtures: [
-      { opponent: "GW33: Tottenham Hotspur", venue: "Away", result: "" },
       { opponent: "GW34: Brentford", venue: "Home", result: "" },
       { opponent: "GW35: Crystal Palace", venue: "Away", result: "" },
       { opponent: "GW36: Leicester City", venue: "Home", result: "" },
